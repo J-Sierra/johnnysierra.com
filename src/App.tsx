51a5d4687b6f5b9c5f5a727faae6e2a523db2e5a
Johnny <gjohnnys@att.net>
2023-05-31 19:02:43 -0700

    5/31/23

--- src/App.tsx
+++ src/App.tsx
@@ -82,19 +82,21 @@ function App() {
                   }}
                 />
               </div>
-
               <p>
-                Hello! My name is Johnny and I make stuff that lives on the
-                internet. <br /> My interest in web design started back in 2011
-                when I started my college career. I have always been fascinated
-                by technology since my childhood, tinkering with everything I
-                could get my hands on. I would take apart TV's, VCR's, vacuums,
-                toys, just to see how they worked and for the challenge of been
-                able to put them back together.
+                Welcome to my personal website! I'm Johnny Sierra, a passionate
+                software developer with a drive for creating innovative and
+                efficient solutions. With 2 years of experience in the industry,
+                I specialize in front-end web development with a focus on React.
+                <br />
+                <br />I am highly skilled in React, React Native, JavaScript,
+                HTML, CSS, and Redux. I actively stay updated with the latest
+                industry trends and emerging technologies. Constantly seeking to
+                expand my knowledge, I'm always eager to explore new frameworks
+                and languages to enhance my skill set.
               </p>
             </div>
           </section>
-          <section id="experience">
+          <section id="experience-section">
             <div className="inner">
               <div
                 style={{
@@ -112,7 +114,109 @@ function App() {
                   }}
                 />
               </div>
-              <div></div>
+              <div
+                className={"work-experience-block"}
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "flex-start",
+                }}
+              >
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                    marginBottom: "-35px",
+                  }}
+                >
+                  <h1>NURO Inc.</h1>
+                  <p>Oct 2019 - May 2023</p>
+                </div>
+                <h4 style={{ color: "#D3D3D382" }}>
+                  Robotic Specialist/Software Development
+                </h4>
+                <div>
+                  <p>
+                    Nuro is an up and coming Robotics Delivery Startup aiming to
+                    bring self-driving Robots to domestic deliveries.
+                  </p>
+                  <ul>
+                    <li>
+                      <strong>Developed -</strong> Worked as part of a team to
+                      implement in-house web tools dedicated to improving
+                      productivity.
+                    </li>
+                    <li>
+                      <strong>Maintained -</strong> Tested and implemented
+                      changes to the autonomous robot platform software.
+                    </li>
+                    <li>
+                      <strong>Documented -</strong> Reported and documented any
+                      edge cases in the software to ensure robustness.
+                    </li>
+                    <li>
+                      <strong>Upkeep -</strong> Maintained the highest level of
+                      safety for the operation of the autonomous robot platform.
+                    </li>
+                    <li>
+                      <strong>Communication -</strong> Ensured efficient and
+                      concise communication among 4 cities of operations.
+                    </li>
+                  </ul>
+                </div>
+              </div>
+
+              <div
+                className={"work-experience-block"}
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "flex-start",
+                }}
+              >
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                    marginBottom: "-35px",
+                  }}
+                >
+                  <h1>R Kirk Enterprises</h1>
+                  <p>August 2017 - August 2019</p>
+                </div>
+                <h4 style={{ color: "#D3D3D382" }}>Software Developer</h4>
+                <div>
+                  <p>
+                    Nuro is an up and coming Robotics Delivery Startup aiming to
+                    bring self-driving Robots to domestic deliveries.
+                  </p>
+                  <ul>
+                    <li>
+                      <strong>Developed -</strong> Worked as part of a team to
+                      implement in-house web tools dedicated to improving
+                      productivity.
+                    </li>
+                    <li>
+                      <strong>Maintained -</strong> Tested and implemented
+                      changes to the autonomous robot platform software.
+                    </li>
+                    <li>
+                      <strong>Documented -</strong> Reported and documented any
+                      edge cases in the software to ensure robustness.
+                    </li>
+                    <li>
+                      <strong>Upkeep -</strong> Maintained the highest level of
+                      safety for the operation of the autonomous robot platform.
+                    </li>
+                    <li>
+                      <strong>Communication -</strong> Ensured efficient and
+                      concise communication among 4 cities of operations.
+                    </li>
+                  </ul>
+                </div>
+              </div>
             </div>
           </section>
         </main>
