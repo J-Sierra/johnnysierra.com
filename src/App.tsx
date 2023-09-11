e7a9a5168edc6bc77543878962bc23639db72722
Johnny Sierra <Gjohnnys@att.net>
2023-09-11 16:31:27 -0500

    9/11/23

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { AiOutlineLinkedin } from "react-icons/ai";
 import { BiCopy } from "react-icons/bi";
