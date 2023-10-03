b6f2c783fe2a6efda5343c0dd074a4a54386db8e
Johnny <gjohnnys@att.net>
2023-10-03 00:45:58 -0700

    Adjusted Tech Balls loading speed

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,11 +1,37 @@
-import { BallCanvas } from "./canvas";
+import { motion } from "framer-motion";
 import { SectionWrapper } from "../HOC/index.js";
 import { technologies } from "../constants";
 import { textVariant } from "../utils/motion.js";
 import { styles } from "../styles.js";
-import { motion } from "framer-motion";
+import { BallCanvas } from "./canvas/index.js";
+import { useEffect, useState } from "react";
 
 const Tech = () => {
+  const [loadingQueue, setLoadingQueue] = useState([...technologies]);
+  const [loadedComponents, setLoadedComponents] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const loadNextBall = async () => {
+      if (loadingQueue.length > 0 && !isLoading) {
+        await new Promise((resolve) => setTimeout(resolve, 800));
+        setIsLoading(true);
+        const icon = loadingQueue[0].icon;
+
+        setLoadedComponents((prev) => [
+          ...prev,
+          <BallCanvas key={icon} icon={icon} />,
+        ]);
+
+        setLoadingQueue((prevQueue) => prevQueue.slice(1));
+
+        setIsLoading(false);
+      }
+    };
+
+    loadNextBall();
+  }, [loadingQueue, isLoading, loadedComponents]);
+
   return (
     <>
       <motion.div variants={textVariant()} className="pb-3">
@@ -13,11 +39,11 @@ const Tech = () => {
         <h2 className={styles.sectionHeadText}>Technologies</h2>
       </motion.div>
       <div className="flex flex-row flex-wrap justify-center gap-10">
-        {technologies.map((technology) => (
-          <div className="w-32 h-32 flex flex-col" key={technology.name}>
-            <BallCanvas icon={technology.icon} />
+        {loadedComponents.map((component, index) => (
+          <div className="w-32 h-32 flex flex-col" key={index}>
+            {component}
             <span className="text-center font-semibold text-sm">
-              {technology.name}
+              {technologies[index].name}
             </span>
           </div>
         ))}
